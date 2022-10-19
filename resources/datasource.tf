fed265e8a972b83df6f7759738ca8339dae5c8e2
Lucas Migliorança <41163225+luumiglioranca@users.noreply.github.com>
2022-10-19 16:41:52 -0300

    Create datasource.tf

---
+++ resources/datasource.tf
@@ -0,0 +1,6 @@
+# Data source responsável por procurar o recurso dentro de sua conta AWS.
+# Você pode alocar o ARN do recurso e for mais fácil.
+
+data "aws_apigatewayv2_api" "waf_for_api_gateway" {
+  api_id = ""
+}
