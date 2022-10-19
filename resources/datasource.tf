86ed65e9fc56d6a76d76c2f282281daa68a39a29
Lucas Migliorança <41163225+luumiglioranca@users.noreply.github.com>
2022-10-19 16:56:35 -0300

    Update datasource.tf

--- resources/datasource.tf
+++ resources/datasource.tf
@@ -1,5 +1,5 @@
 # Data source responsável por procurar o recurso dentro de sua conta AWS.
-# Você pode alocar o ARN do recurso e for mais fácil.
+# Você pode alocar o ARN do recurso se for mais fácil.
 
 data "aws_apigatewayv2_api" "waf_for_api_gateway" {
   api_id = ""
